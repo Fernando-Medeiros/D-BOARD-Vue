6ee0a9cb8ea97b6fb05141e00f07eb50857d1e43
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 20:46:46 -0300

    feat(comp): IconLogo

---
+++ src/components/icons/IconLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <RouterLink to="/">
+        <img src="/resources/logo.png" alt="logo" />
+    </RouterLink>
+</template>
+
+<style scoped>
+img {
+    width: auto;
+    height: auto;
+}
+</style>
