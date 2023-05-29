ffa3dceece0dc29a6c67c1901cd0bda2665579a8
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 20:47:26 -0300

    feat(comp): IconLinkedin

---
+++ src/components/icons/IconLinkedin.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const link = 'https://www.linkedin.com/in/fernando-de-alvarenga-medeiros/'
+</script>
+
+<template>
+    <a :href="link" target="_blank"><img src="/resources/linkedin.svg" alt="github" /></a>
+</template>
+
+<style scoped>
+img {
+    width: 38px;
+    height: 38px;
+}
+</style>
