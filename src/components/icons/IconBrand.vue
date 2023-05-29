5002ada7610b1592bce3692eb92bb3f4a1380c94
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 20:46:30 -0300

    feat(comp): IconBrand

---
+++ src/components/icons/IconBrand.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <RouterLink to="/">
+        <img src="/resources/brand.png" alt="brand" />
+    </RouterLink>
+</template>
+
+<style scoped>
+img {
+    width: auto;
+    height: auto;
+}
+</style>
