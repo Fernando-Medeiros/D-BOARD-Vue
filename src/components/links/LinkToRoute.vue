38b156e2d93aa0f64f64d7980e1de1da5a466de5
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 20:48:07 -0300

    feat(comp): LinkToRoute

---
+++ src/components/links/LinkToRoute.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+defineProps<{ route: string; label: string }>()
+</script>
+
+<template>
+    <RouterLink :to="route" class="link-to-route">{{ label }}</RouterLink>
+</template>
+
+<style scoped>
+.link-to-route {
+    text-decoration: none;
+}
+.link-to-route:hover {
+    color: green;
+    text-decoration: underline 2px;
+    transition: all 1s;
+}
+.link-to-route.router-link-exact-active {
+    text-decoration: underline 3px;
+    text-decoration-color: green;
+}
+</style>
