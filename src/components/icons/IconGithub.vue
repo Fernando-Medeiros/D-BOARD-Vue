0373685a0cfa0f5f38848a428a2c93df8840dc64
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 20:47:06 -0300

    feat(comp): IconGithub

---
+++ src/components/icons/IconGithub.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const link = 'https://github.com/Fernando-Medeiros'
+</script>
+
+<template>
+    <a :href="link" target="_blank"><img src="/resources/github.svg" alt="github" /></a>
+</template>
+
+<style scoped>
+img {
+    width: 38px;
+    height: 38px;
+}
+</style>
