7e35d11536825758fd7a47f997d3efca074eb4ab
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 18:57:54 -0300

    fix: rm HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from 'views/HomeView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +6,8 @@ const router = createRouter({
         {
             path: '/',
             children: [
-                { path: '', name: 'home', component: HomeView },
                 {
-                    path: 'signin',
+                    path: '',
                     name: 'signin',
                     component: () => import('views/SignInView.vue')
                 },
