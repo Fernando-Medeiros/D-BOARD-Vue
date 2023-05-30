dd9432e890ac95b46379d7bdd577333e3697a327
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:40:11 -0300

    feat(comp): InputCheckBox

---
+++ src/components/forms/InputCheckBox.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ label: string }>()
+
+const inputData = ref(false)
+
+defineExpose({ inputData })
+</script>
+
+<template>
+    <div>
+        <input v-model="inputData" type="checkbox" />
+        <label :for="label">{{ label }}</label>
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    gap: 10px;
+    width: max-content;
+    align-items: center;
+}
+input {
+    height: 1.2rem;
+    width: 1.2rem;
+}
+label {
+    font-size: 1rem;
+    font-weight: 300;
+}
+</style>
