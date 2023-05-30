9fbe71f726b16777266f726009f4af7c35eb06d3
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:43:19 -0300

    feat(view): SignIn -> Forms

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,5 +1,79 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import FormTemplate from 'comps/forms/FormTemplate.vue'
+import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
+import InputCheckBoxVue from 'comps/forms/InputCheckBox.vue'
+import InputSubmit from 'comps/forms/InputSubmit.vue'
+import IconGithub from 'comps/icons/IconGithub.vue'
+import IconLinkedin from 'comps/icons/IconLinkedin.vue'
+import LinkToRoute from 'comps/links/LinkToRoute.vue'
 
-<template><div>signin</div></template>
+const email = ref()
+const password = ref()
+const checkbox = ref()
 
-<style scoped></style>
+function signIn() {}
+</script>
+
+<template>
+    <div>
+        <FormTemplate @submit.prevent="signIn">
+            <template v-slot:one>
+                <InputGenericTypes
+                    ref="email"
+                    :label="'Email'"
+                    :type="'email'"
+                    :placeholder="'email@example.com'"
+                />
+            </template>
+
+            <template v-slot:two>
+                <InputGenericTypes
+                    ref="password"
+                    :label="'Senha'"
+                    :type="'password'"
+                    :placeholder="'*********'"
+                />
+            </template>
+
+            <template v-slot:three>
+                <InputCheckBoxVue ref="checkbox" :label="'lembrar'" />
+            </template>
+
+            <template v-slot:four>
+                <InputSubmit :label="'entrar'" />
+            </template>
+
+            <template v-slot:five>
+                <div class="form-options">
+                    <span class="options-icons">
+                        <IconGithub />
+                        <IconLinkedin />
+                    </span>
+
+                    <span class="options-links">
+                        <LinkToRoute :route="'/signup'" :label="'inscrever-se'" />
+                        <LinkToRoute :route="'/password/recover'" :label="'esqueceu a senha?'" />
+                    </span>
+                </div>
+            </template>
+        </FormTemplate>
+    </div>
+</template>
+
+<style scoped>
+.form-options {
+    display: flex;
+    align-items: end;
+    justify-content: space-between;
+}
+.options-icons {
+    display: flex;
+    gap: 1rem;
+}
+.options-links {
+    display: grid;
+    gap: 10px;
+    text-align: right;
+}
+</style>
