75ccc79063aae3a457df686723b0840297f673a2
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:39:50 -0300

    feat(comp): InputGenericTypes

---
+++ src/components/forms/InputGenericTypes.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ label: string; placeholder: string; type: string }>()
+
+const inputData = ref()
+
+defineExpose({ inputData })
+</script>
+
+<template>
+    <span>
+        <label :for="label">{{ label }}</label>
+        <input
+            v-model="inputData"
+            :type="type"
+            :autocomplete="type"
+            :placeholder="placeholder"
+            :required="true"
+        />
+    </span>
+</template>
+
+<style scoped>
+span {
+    display: grid;
+    gap: 1.5rem;
+}
+input {
+    width: 100%;
+    height: 40px;
+    font-size: 1.1rem;
+    padding-inline: 5px;
+    box-sizing: border-box;
+    border: 1px solid;
+    border-radius: 5px;
+    border-color: #42d09559;
+}
+input:focus {
+    color: black;
+    background-color: #d9d9d9;
+}
+
+@media (max-width: 780px) {
+}
+</style>
