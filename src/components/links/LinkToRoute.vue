72061313f1f7e41b6b5d3752150a8c60dcad298c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:38:10 -0300

    refactor(comp): LinkToRoute -> colors

--- src/components/links/LinkToRoute.vue
+++ src/components/links/LinkToRoute.vue
@@ -12,12 +12,11 @@ defineProps<{ route: string; label: string }>()
     text-decoration: none;
 }
 .link-to-route:hover {
-    color: green;
-    text-decoration: underline 2px;
     transition: all 1s;
+    text-decoration: underline 2px;
 }
 .link-to-route.router-link-exact-active {
     text-decoration: underline 3px;
-    text-decoration-color: green;
+    text-decoration-color: #42d0959f;
 }
 </style>
