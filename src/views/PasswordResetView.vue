0393f175512511a353eb9f549fb4b154e588cadb
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:44:44 -0300

    feat(view): PasswordReset -> Forms

--- src/views/PasswordResetView.vue
+++ src/views/PasswordResetView.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import FormTemplate from 'comps/forms/FormTemplate.vue'
+import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
+import InputSubmit from 'comps/forms/InputSubmit.vue'
 
-<template><div>reset</div></template>
+const password = ref()
+const confirmPassword = ref()
+
+function resetPassword() {}
+</script>
+
+<template>
+    <div>
+        <FormTemplate @submit.prevent="resetPassword">
+            <template v-slot:one>
+                <InputGenericTypes
+                    ref="password"
+                    :label="'Senha'"
+                    :type="'password'"
+                    :placeholder="'*********'"
+                />
+            </template>
+
+            <template v-slot:two>
+                <InputGenericTypes
+                    ref="confirmPassword"
+                    :label="'Confirme a Senha'"
+                    :type="'password'"
+                    :placeholder="'*********'"
+                />
+            </template>
+
+            <template v-slot:three>
+                <InputSubmit :label="'redefinir'" />
+            </template>
+        </FormTemplate>
+    </div>
+</template>
 
 <style scoped></style>
