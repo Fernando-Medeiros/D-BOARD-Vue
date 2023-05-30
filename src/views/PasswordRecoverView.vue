bd186c2ef2a81e1cac50d03692a73e8e0a53d6b2
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:44:20 -0300

    feat(view): PasswordRecover -> Forms

--- src/views/PasswordRecoverView.vue
+++ src/views/PasswordRecoverView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import FormTemplate from 'comps/forms/FormTemplate.vue'
+import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
+import InputSubmit from 'comps/forms/InputSubmit.vue'
 
-<template><div>recover</div></template>
+const email = ref()
+
+function recoverPassword() {}
+</script>
+
+<template>
+    <div>
+        <FormTemplate @submit.prevent="recoverPassword">
+            <template v-slot:one>
+                <InputGenericTypes
+                    ref="email"
+                    :label="'Email'"
+                    :type="'email'"
+                    :placeholder="'email@example.com'"
+                />
+            </template>
+
+            <template v-slot:two>
+                <InputSubmit :label="'enviar'" />
+            </template>
+        </FormTemplate>
+    </div>
+</template>
 
 <style scoped></style>
