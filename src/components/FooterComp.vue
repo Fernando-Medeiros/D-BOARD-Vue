d1198038d980977222a5139b761f9d106c133f38
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:41:00 -0300

    feat(comp): FooterComp

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header v-show="false" class="footer-bar"></header>
+</template>
+
+<style scoped>
+.footer-bar {
+    display: flex;
+    padding: 5px;
+    align-items: center;
+    justify-content: space-between;
+    border-top: 2px solid #363636;
+}
+</style>
