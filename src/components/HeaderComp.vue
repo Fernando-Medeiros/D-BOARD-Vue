99947dc1ee7cc7a3e719ff41a46d852531e00af5
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-29 21:40:42 -0300

    feat(comp): HeaderComp

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import IconBrand from 'comps/icons/IconBrand.vue'
+import LinkToRoute from 'comps/links/LinkToRoute.vue'
+</script>
+
+<template>
+    <header class="nav-bar">
+        <IconBrand />
+        <nav>
+            <LinkToRoute :route="'/signup'" :label="'Inscrever-se'" />
+            <LinkToRoute :route="'/'" :label="'Entrar'" />
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+.nav-bar {
+    display: flex;
+    padding: 5px;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 2px solid #363636;
+
+    margin-bottom: 4rem;
+}
+.nav-bar nav {
+    display: flex;
+    gap: 2rem;
+}
+</style>
