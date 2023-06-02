66ac9325a5c6d9033a01bc8647ba5445d6128850
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-01 23:02:31 -0300

    feat(core): PasswordService

---
+++ src/core/services/password/password.service.ts
@@ -0,0 +1,20 @@
+import AxiosMethods from 'libs/axios'
+
+const API = String(process.env.VITE_API_PASSWORD)
+
+export default class PasswordService {
+    public static async recover(form: RecoverPasswordDto) {
+        return AxiosMethods.post({ url: API, data: form })
+    }
+
+    public static async reset(token: string, form: UpdatePasswordDto) {
+        return AxiosMethods.put({ url: API.concat('/', token), data: form })
+    }
+
+    public static async update(form: UpdatePasswordDto) {
+        return AxiosMethods.patch({ url: API, data: form })
+    }
+}
+
+type RecoverPasswordDto = { email: string }
+type UpdatePasswordDto = { password: string }
