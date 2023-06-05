e41113bb31ee0a0981314ecd1cbce4431e51a132
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-05 19:18:53 -0300

    feat(view): Impl PasswordResetView

--- src/views/PasswordResetView.vue
+++ src/views/PasswordResetView.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import router from 'routes/index'
+import AlertMessage from 'utils/alert.message'
+import PasswordService from 'services/password/password.service'
 import FormTemplate from 'comps/forms/FormTemplate.vue'
 import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
 import InputSubmit from 'comps/forms/InputSubmit.vue'
 
+onBeforeMount(() => {
+    const { token } = useRoute().query
+    if (String(token).length < 110) return router.push({ name: 'signin' })
+})
+
 const password = ref()
 const confirmPassword = ref()
 
-function resetPassword() {}
+async function resetPassword() {
+    const { token } = useRoute().query
+
+    const { message, status } = await PasswordService.reset(String(token), {
+        password: password.value.data
+    })
+
+    if (status === 204) {
+        return router.push({ name: 'signin' })
+    } else {
+        AlertMessage.showAlertWithTimer(message, 'warning')
+    }
+}
 </script>
 
 <template>
