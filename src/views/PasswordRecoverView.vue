4a4a7c071324149c4036036bf53e8a33603d7e71
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-05 19:18:32 -0300

    feat(view): Impl PasswordRecoverView

--- src/views/PasswordRecoverView.vue
+++ src/views/PasswordRecoverView.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import router from 'routes/index'
+import AlertMessage from 'utils/alert.message'
+import PasswordService from 'services/password/password.service'
 import FormTemplate from 'comps/forms/FormTemplate.vue'
 import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
 import InputSubmit from 'comps/forms/InputSubmit.vue'
 
 const email = ref()
 
-function recoverPassword() {}
+async function recoverPassword() {
+    const { message, status } = await PasswordService.recover({ email: email.value.data })
+
+    if (status === 201) {
+        AlertMessage.showAlertWithTimer(message, 'success')
+        return router.push({ name: 'signin' })
+    } else {
+        AlertMessage.showAlertWithTimer(message, 'warning')
+    }
+}
 </script>
 
 <template>
