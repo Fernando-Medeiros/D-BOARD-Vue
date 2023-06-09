9d8e9aae86a77ed06cb10733933a5f4cd5d590a6
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-09 18:20:02 -0300

    feat(routers): add /profile && router constraints

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SessionStorage from 'utils/session.storage'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,11 +35,36 @@ const router = createRouter({
                 {
                     path: 'update',
                     name: 'update',
-                    component: () => import('views/PasswordUpdateView.vue')
+                    component: () => import('views/PasswordUpdateView.vue'),
+                    meta: {
+                        requiresAuth: true
+                    }
                 }
+            ]
+        },
+        {
+            path: '/profile',
+            meta: {
+                requiresAuth: true
+            },
+            children: [
+                { path: '', name: 'profile', component: () => import('views/ProfileView.vue') }
             ]
         }
     ]
+})
+
+router.beforeEach((to) => {
+    const { requiresAuth } = to.meta
+
+    if (to.name === 'reset') {
+        const token = String(to.query?.token)
+        if (token.length < 110) return { name: 'signin' }
+    }
+
+    if (requiresAuth && !SessionStorage.getToken()) return { name: 'signin' }
+
+    if (!requiresAuth && SessionStorage.getToken()) return { name: 'profile' }
 })
 
 export default router
