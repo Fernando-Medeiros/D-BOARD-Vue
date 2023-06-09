d30f541191af0ff61507d9da1f22b112adec38c2
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-09 13:59:15 -0300

    feat(view): PasswordUpdateView -> add input Rules

--- src/views/PasswordUpdateView.vue
+++ src/views/PasswordUpdateView.vue
@@ -2,11 +2,13 @@
 import { ref } from 'vue'
 import router from 'routes/index'
 import AlertMessage from 'utils/alert.message'
+import InputRegex from 'utils/input.regex'
 import PasswordService from 'services/password/password.service'
 import FormTemplate from 'comps/forms/FormTemplate.vue'
 import InputGenericTypes from 'comps/forms/InputGenericTypes.vue'
 import InputSubmit from 'comps/forms/InputSubmit.vue'
 
+const submitForm = ref(false)
 const password = ref()
 const confirmPassword = ref()
 
@@ -19,6 +21,20 @@ async function updatePassword() {
         AlertMessage.showAlertWithTimer(message, 'warning')
     }
 }
+
+function passwordRules(): string {
+    const { result, msg } = InputRegex.testPassword(password.value?.data)
+    submitForm.value = result
+    return password.value?.data && result === false ? msg : ''
+}
+
+function confirmPasswordRules(): string {
+    const result = confirmPassword.value?.data === password.value?.data
+    submitForm.value = result
+    return confirmPassword.value?.data && result === false
+        ? 'Confirme a senha, elas devem ser idênticas.'
+        : ''
+}
 </script>
 
 <template>
@@ -30,6 +46,9 @@ async function updatePassword() {
                     :label="'Senha'"
                     :type="'password'"
                     :placeholder="'*********'"
+                    :autocomplete="'new-password'"
+                    :required="true"
+                    :rules="passwordRules"
                 />
             </template>
 
@@ -39,6 +58,9 @@ async function updatePassword() {
                     :label="'Confirme a Senha'"
                     :type="'password'"
                     :placeholder="'*********'"
+                    autocomplete="'new-password'"
+                    :required="true"
+                    :rules="confirmPasswordRules"
                 />
             </template>
 
@@ -48,5 +70,3 @@ async function updatePassword() {
         </FormTemplate>
     </div>
 </template>
-
-<style scoped></style>
