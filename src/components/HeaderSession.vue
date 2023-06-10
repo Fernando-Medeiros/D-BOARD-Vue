fa8b305787ae7afecef909516bb8c966eb9748e7
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-09 21:37:16 -0300

    feat(comp): HeaderSession

---
+++ src/components/HeaderSession.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import IconMenu from './icons/sidebar/IconMenu.vue'
+</script>
+
+<template>
+    <header class="nav-bar">
+        <IconMenu />
+        <nav></nav>
+    </header>
+</template>
+
+<style scoped>
+.nav-bar {
+    display: flex;
+    padding: 5px;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 2px solid #363636;
+
+    margin-bottom: 4rem;
+}
+.nav-bar nav {
+    display: flex;
+    gap: 2rem;
+}
+</style>
