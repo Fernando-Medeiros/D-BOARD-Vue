28744d777e3dbb610fb06c602000c44b35416d7b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-09 21:31:45 -0300

    feat(routers): add /logout; /dashboard; /tasks; /profile/settigs && Logout Constraint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SessionStorage from 'utils/session.storage'
+import { useSessionGlobalState } from 'composable/useGlobalStates'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            children: [
-                {
-                    path: '',
-                    name: 'signin',
-                    component: () => import('views/SignInView.vue')
-                },
-                {
-                    path: 'signup',
-                    name: 'signup',
-                    component: () => import('views/SignUpView.vue')
-                }
-            ]
+            name: 'signin',
+            component: () => import('views/SignInView.vue')
         },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: () => import('views/SignUpView.vue')
+        },
+        {
+            path: '/logout',
+            name: 'logout',
+            redirect: '',
+            meta: { requiresAuth: true }
+        },
+
         {
             path: '/password',
             children: [
@@ -36,19 +38,49 @@ const router = createRouter({
                     path: 'update',
                     name: 'update',
                     component: () => import('views/PasswordUpdateView.vue'),
-                    meta: {
-                        requiresAuth: true
-                    }
+                    meta: { requiresAuth: true }
                 }
             ]
         },
+
         {
             path: '/profile',
-            meta: {
-                requiresAuth: true
-            },
+            meta: { requiresAuth: true },
             children: [
-                { path: '', name: 'profile', component: () => import('views/ProfileView.vue') }
+                {
+                    path: '/profile',
+                    name: 'profile',
+                    component: () => import('views/ProfileView.vue')
+                },
+                {
+                    path: 'settings',
+                    name: 'settings',
+                    component: () => import('views/ProfileSettingsView.vue')
+                }
+            ]
+        },
+
+        {
+            path: '/dashboard',
+            meta: { requiresAuth: true },
+            children: [
+                {
+                    path: '/dashboard',
+                    name: 'dashboard',
+                    component: () => import('views/DashBoardView.vue')
+                }
+            ]
+        },
+
+        {
+            path: '/tasks',
+            meta: { requiresAuth: true },
+            children: [
+                {
+                    path: '/tasks',
+                    name: 'tasks',
+                    component: () => import('views/TasksView.vue')
+                }
             ]
         }
     ]
@@ -57,14 +89,19 @@ const router = createRouter({
 router.beforeEach((to) => {
     const { requiresAuth } = to.meta
 
+    if (requiresAuth && !useSessionGlobalState().value) return { name: 'signin' }
+
+    if (!requiresAuth && useSessionGlobalState().value) return { name: 'profile' }
+
     if (to.name === 'reset') {
         const token = String(to.query?.token)
         if (token.length < 110) return { name: 'signin' }
     }
 
-    if (requiresAuth && !SessionStorage.getToken()) return { name: 'signin' }
-
-    if (!requiresAuth && SessionStorage.getToken()) return { name: 'profile' }
+    if (to.name === 'logout') {
+        useSessionGlobalState().value = null
+        return { name: 'signin' }
+    }
 })
 
 export default router
